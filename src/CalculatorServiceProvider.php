3e62e01669244cdbb01a993fded8869594a64978
Farrukh Ayyaz <farrukhayyaz@gmail.com>
2019-11-06 16:19:28 +0500

    Calculator

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Devdojo\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/calculator', function() {
+  return 'Hello form calculator package';
+});
